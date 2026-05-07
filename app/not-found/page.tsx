a323a3d47e7a76f524d81e222717a7cf82d86fdb
Patrick Namegni <49642047+CodeShadowing95@users.noreply.github.com>
2026-05-07 10:47:25 +0200

    feat: Set up shadcn/ui & update layout for portfolio

--- app/not-found/page.tsx
+++ app/not-found/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div className='flex flex-col items-center justify-center h-full'>
+      <p className='text-lg font-sans'>Oops! The page you{`'`}re looking for doesn{`'`}t exist.</p>
+      <h1 className='text-4xl font-bold font-sans'>404 - Page Not Found</h1>
+    </div>
   )
 }
 
